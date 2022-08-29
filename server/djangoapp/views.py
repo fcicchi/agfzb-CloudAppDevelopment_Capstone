5b7268c3a4a00607f51c6dc45b45ac3712d2b65f
Federico Cicchi <federico.cicchi@xyzreality.com>
2022-08-29 21:59:19 +0100

    Capstone project - Week 2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,25 +14,75 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create an `about` view to render a static about page
+# Create your views here.
+
 def about(request):
-    return render(request, 'djangoapp/about.html')
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
-# Create a `contact` view to return a static contact page
 def contact(request):
-    return render(request, 'djangoapp/contact.html')
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {} 
+    if request.method == "POST":
+        user = request.POST['user']
+        password = request.POST['password']
+        user = authenticate(username=user, password=password)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            resp =  redirect('djangoapp:index')
+            resp['Location'] += '?msg=login_failed'
+            return resp
+    else:
+        return redirect('djangoapp:index')
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def logout_request(request):
+    resp =  redirect('djangoapp:index')
+    resp['Location'] += '?msg=logout&user=' + request.user.username
+    logout(request)
+    return resp
+
+def registration_request(request):
+    context = {}
+    return render(request, 'djangoapp/registration.html', context)
+
+def signup_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['user']
+        password = request.POST['password']
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        user_exist = False
+
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} is new user".format(username))
+
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+            login(request, user)
+            return redirect('djangoapp:index')
+
+        else:
+            resp =  redirect('djangoapp:register')
+            resp['Location'] += '?signin=exist'
+            logout(request)
+            return resp
+
+    else:
+        return redirect('djangoapp:register')
+
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -48,4 +98,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
