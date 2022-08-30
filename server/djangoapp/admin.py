ed68a9ea7c821de012aa22f9cc1b69550ed0bde2
Federico Cicchi <federico.cicchi@xyzreality.com>
2022-08-30 22:46:05 +0100

    Capstone project - Week 3 - Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from .models import CarModel, CarMake
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+        fields = ['model_id', 'name', 'dealer_id','car_type','year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+        fields = ['name', 'desc']
+        inlines = [CarModelInline]
 
-# Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
