fa04b0aeea0223ed117a656f85b2e34094d896e4
Federico Cicchi <federico.cicchi@xyzreality.com>
2022-08-30 23:51:03 +0100

    Capstone project - Week 3 - WIP

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,21 +1,23 @@
 import requests
 import json
-# import related models here
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
+# Watson NLU API configuration values
+watson_nlu_api_base_url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/bc906088-fbeb-40cd-abd5-3738474659aa"
+watson_nlu_api_key = "4aBlw6qGio3BCXSIjb6ReuOYTAsAQQBbd-pWUtfsVS7W"
+watson_nlu_api_version = '2021-08-01'
 
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
 
     try:
         # no authentication GET
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                params=kwargs)
+        if api_key:
+            response = requests.get(url, headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth('apikey', api_key), params=kwargs)
+        else:
+            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -61,6 +63,7 @@ def get_dealers_from_cf(url, **kwargs):
                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
                                    short_name=dealer_doc["short_name"],
                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
+
             results.append(dealer_obj)
 
     return results
@@ -104,10 +107,6 @@ def get_dealers_by_state(url, state, **kwargs):
     return results
 
 
-# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-# def get_dealer_by_id_from_cf(url, dealerId):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a DealerView object list
 def get_dealer_reviews_from_cf(url,dealerId):
     results = []
     # Call get_request with a URL parameter
@@ -123,10 +122,24 @@ def get_dealer_reviews_from_cf(url,dealerId):
                                       purchase_date=review["purchase_date"],car_make=review["car_make"],
                                       car_model=review["car_model"],car_year=review["car_year"],
                                       sentiment=sentiment)
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
             results.append(review_obj)
     return results
 
-# Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
-# - Call get_request() with specified arguments
-# - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(text):
+    url = watson_nlu_api_base_url + "/analyze"
+    api_key = watson_nlu_api_key
+    version = watson_nlu_api_version   
+    features = {"sentiment" : {}}    
+    params = dict()
+    params["text"] = text
+    params["version"] = version
+    params["features"] = features
+    params["return_analyzed_text"] = True
+
+    response = get_request(url, params=params, headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth('apikey', api_key))
+    if "error" in response:
+        return "neutral"
+    else:
+        text_sentiment = response["sentiment"]["document"]["label"]
+        return text_sentiment
